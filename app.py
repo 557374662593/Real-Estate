ef0690437a6cb06dc52229278f69e9a887014d1b
557374662593 <yogeshkhandare20@gmail.com>
2025-09-08 21:42:17 +0530

    updated app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-
+import os
 app = Flask(__name__)
 
 
@@ -49,5 +49,9 @@ def predict():
         return jsonify({'error': str(e)}), 400
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
